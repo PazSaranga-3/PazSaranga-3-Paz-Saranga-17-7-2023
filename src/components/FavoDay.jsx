e895e3bd95ea6e0a590ef903810c5e8b94cc0edc
Paz Saranga <133895239+PazSaranga-3@users.noreply.github.com>
2023-07-19 23:14:06 +0300

    Best

---
+++ src/components/FavoDay.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function FavoDay() {
+  return (
+    <Link to=''>
+      <div onClick>
+
+      </div>
+    </Link>
+  )
+}
