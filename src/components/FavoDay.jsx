ceca4e5693ebdfcd095efc206c77e954f2edcdf4
Paz Saranga <133895239+PazSaranga-3@users.noreply.github.com>
2023-07-20 12:31:26 +0300

    Help

--- src/components/FavoDay.jsx
+++ src/components/FavoDay.jsx
@@ -9,31 +9,19 @@ export default function FavoDay(props) {
   const [temperature, setTemperature] = useState(null);
   let nav = useNavigate();
 
-  const cityTemp = async(city) => {            //return the Temp of today
-    try {
-      
-      let url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=RmuQRSmtJUJaipmQEdpFo1grsGt2abOF&q=${city}`
-      let cityResponse = await axios.get(url)
-      let cityK = cityResponse.data[0].Key
-      
-      let url2 = `http://dataservice.accuweather.com/currentconditions/v1/${cityK}?apikey=RmuQRSmtJUJaipmQEdpFo1grsGt2abOF&metric=true`
-      let daysResponse = await axios.get(url2)
-      // console.log(daysResponse);
-      let temperature = daysResponse.data[0].Temperature.Metric.Value;
-      store.dispatch({ type: 'updateTemp', payload: temperature });
-      return store.getState().reducerTemp
-    }
-    catch (error) {
-      console.error('Error fetching data:', error);
-    }
-    
-  }
-
+  
   useEffect(() => {
     async function fetchCityTemperature() {
       try {
-        const temp = await cityTemp(props.city);
+        let url = `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=RmuQRSmtJUJaipmQEdpFo1grsGt2abOF&q=${props.city}`
+        let cityResponse = await axios.get(url)
+        let cityK = cityResponse.data[0].Key
+        
+        let url2 = `http://dataservice.accuweather.com/currentconditions/v1/${cityK}?apikey=RmuQRSmtJUJaipmQEdpFo1grsGt2abOF&metric=true`
+        let daysResponse = await axios.get(url2)
+        let temp = daysResponse.data[0].Temperature.Metric.Value;
         setTemperature(temp);
+        console.log(temperature);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
